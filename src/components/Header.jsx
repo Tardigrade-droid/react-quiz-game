3654c5d46d3818a40ba33e3ccf24908470f40b96
Tardigrade-droid <percymbuku@gmail.com>
2022-04-12 06:55:30 +0100

    new changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
+import {useState} from 'react'
+
 const Header = ()=>{
+	const [btnState, setBtnState] = useState('active')
 	return <header>
 		<h1>quiz-app</h1>
+		<div className={`btn ${btnState}`} onclick={setBtnState("")}><span></span></div>
+		
 		</header>
 }
 
